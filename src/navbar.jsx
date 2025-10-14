896ec5c79554ef9e96b0545420135dc078986b90
Jason <jason.ye914@gmail.com>
2025-10-14 01:30:38 -0400

    commits on homepage

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,10 +1,11 @@
- 
+import './CSS/navbar.css'; 
 import { Link} from "react-router-dom";
 
 function navbar(){
     return (
         <nav className="navbar">
             <div className="navbar-brand">
+                <p className="name">Jason Ye</p>
                 <Link to="/" className="home-link"> Home </Link> 
                 <Link to="/about" className="about-link"> About </Link> 
                 <Link to="/project" className="project-link"> Project </Link> 
